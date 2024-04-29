e9ce5f607c7d47b77b1ca5efdac3208e33ca52d1
Dmitrii Goroshuk <goroshuk@yandex.ru>
2024-04-29 14:23:55 +0300

    task 2 is done

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,95 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { Currencies } from "./data/currencies";
+  import { onMount } from "svelte";
+
+  let from: number | string = 0;
+  let to: number | string = 0;
+  let fromCurrency: string = "RUB";
+  let toCurrency: string = "USD";
+  const rates: object = { conversion_rates: {} };
+  let isDownloading: boolean = false;
+
+  onMount(async () => {
+    await fetchConversionRates(toCurrency);
+  });
+
+  async function fetchConversionRates(currency: string) {
+    const url = `https://v6.exchangerate-api.com/v6/f763d3a3fca713780af23df5/latest/${currency}`;
+    isDownloading = true;
+
+    try {
+      const responce = await fetch(url);
+
+      const json = await responce.json();
+
+      rates.conversion_rates = { ...json?.conversion_rates };
+      console.log(rates);
+    } catch (e) {
+      console.log(e);
+    }
+    isDownloading = false;
+  }
+
+  function calculateTo() {
+    const usdNumber =
+      (1 / rates?.conversion_rates?.[fromCurrency]) * Number(from);
+
+    to = (rates?.conversion_rates?.[toCurrency] * usdNumber).toFixed(2);
+  }
+  function calculateFrom() {
+    const usdNumber = (1 / rates?.conversion_rates?.[toCurrency]) * Number(to);
+
+    from = (rates?.conversion_rates?.[fromCurrency] * usdNumber).toFixed(2);
+  }
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <a href="https://www.exchangerate-api.com">Rates By Exchange Rate API</a>
+    {#if !isDownloading}
+      <div class="input_wrapper">
+        <input
+          placeholder="from"
+          type="number"
+          bind:value={from}
+          on:input={calculateTo}
+        />
+        <input
+          placeholder="to"
+          type="number"
+          bind:value={to}
+          on:input={calculateFrom}
+        />
+      </div>
+      <select
+        on:change={(event) => {
+          fromCurrency = event?.target?.value;
+          to = 0;
+          from = 0;
+        }}
+        >{#each Object.entries(Currencies) as [currencyCode, currencyName]}
+          <option value={currencyCode} selected={currencyCode === fromCurrency}
+            >{currencyName}</option
+          >
+        {/each}
+      </select>
+      <select
+        on:change={(event) => {
+          toCurrency = event?.target?.value;
+          to = 0;
+          from = 0;
+        }}
+        >{#each Object.entries(Currencies) as [currencyCode, currencyName]}
+          <option value={currencyCode} selected={currencyCode === toCurrency}
+            >{currencyName}</option
+          >
+        {/each}
+      </select>
+    {:else}
+      <div>Currency conversion rates is fetching. Please wait for a while.</div>
+    {/if}
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
